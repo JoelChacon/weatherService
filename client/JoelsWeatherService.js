2a6a10fcf32599783b90a760cef87e9e1f9bb3e7
Joel G. Chacon <taktic11@gmail.com>
2017-02-05 09:50:18 -0600

    looks

--- client/JoelsWeatherService.js
+++ client/JoelsWeatherService.js
@@ -1,17 +1,19 @@
-angular.module('myApp').service('JoelsWeatherService', function($http, $q){
+angular.module('myApp').factory('JoelsWeatherService', function($http, $q){
 
-	this.getWeather = function(city) {
-		var deferred = $q.defer();
+	return {
+		getWeather: function(city) {
+			var deferred = $q.defer();
 
-		$http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + ',us&units=imperial&appid=bf1d49cc7398cc3d05ce0f3e3ec2134c'
-		).then(function(response){
-			var weatherData = response.data.list;
-			var weatherList = [];
+			$http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + 
+				',us&units=imperial&appid=bf1d49cc7398cc3d05ce0f3e3ec2134c')
+				.then(function(response){
+				// console.log("1stResponse", response);
+				var weatherData = response.data.list;
+				var weatherList = [];
 				function formatDate(timestamp) {
-					console.log(timestamp);
 					var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 					var d = new Date(Number.parseInt(timestamp) * 1000);
-						return d.toLocaleString('en-US', options);
+					return d.toLocaleString('en-US', options);
 				}
 				for (var i = 0; i < weatherData.length; i += 8) {
 					weatherList.push({
@@ -20,9 +22,10 @@ angular.module('myApp').service('JoelsWeatherService', function($http, $q){
 						desc: weatherData[0].weather[0].description
 					});
 				}
-			deferred.resolve(weatherList);
-			console.log("1stResponse", response);
-		});
-		return deferred.promise;
-	}
+				deferred.resolve(weatherList);
+				// console.log("2ndResponse", response);
+			});
+			return deferred.promise;
+		}
+	}	
 });
--- client/myMainController.js
+++ client/myMainController.js
@@ -1,9 +1,9 @@
 angular.module('myApp').controller("myMainController", function($scope, JoelsWeatherService){
 	
-	$scope.functionCall =function() {
+	$scope.functionCall = function() {
 		JoelsWeatherService.getWeather($scope.getWeatherFromAPI)
 		.then(function(response){
-				console.log('2ndResponse', response)
+				console.log(response)
 			$scope.weatherObj = response;
 		})  
 	}
